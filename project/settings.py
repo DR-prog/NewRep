f1d456c685cc79599eacb9f67a5bf65ba61a37d1
DR-prog <76442818+DR-prog@users.noreply.github.com>
2020-12-23 18:23:29 +0300

    Add files via upload

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -121,3 +122,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL='home'
+LOGOUT_REDIRECT_URL='home'
